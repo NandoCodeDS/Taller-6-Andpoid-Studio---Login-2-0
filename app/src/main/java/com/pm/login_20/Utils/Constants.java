79ef6c68c2c044cf755571e6f8f0e8a3798bff3f
NandoCodeDS <99371351+NandoCodeDS@users.noreply.github.com>
2022-10-14 22:11:16 -0500

    Update Constants.java

--- app/src/main/java/com/pm/login_20/Utils/Constants.java
+++ app/src/main/java/com/pm/login_20/Utils/Constants.java
@@ -4,7 +4,11 @@ public class Constants {
     //public static final String BASE_URL = "http://localhost:8080/";
 
     //public static final String BASE_URL = "http://192.168.56.1:5432/";
-    public static final String BASE_URL = "http://192.168.56.1:8080/postgres/";
+    //public static final String BASE_URL = "http://192.168.56.1:8080/postgres/";
+    //public static final String BASE_URL = "http://10.0.2.2:8080/postgres/";
+    //public static final String BASE_URL = "http://10.0.2.2:8080/";
+    //public static final String BASE_URL = "http://192.168.1.104:8080/";
+    public static final String BASE_URL = "http://192.168.1.104:8080/postgres/";
 
 
 }
